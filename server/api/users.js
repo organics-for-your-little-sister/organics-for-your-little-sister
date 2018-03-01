a2671b33d729c01080e05b167452a8a002006340
Sarah Wooten <snwooten@gmail.com>
2018-03-01 13:18:45 -0500

    updated user routes pending testing

--- server/api/users.js
+++ server/api/users.js
@@ -1,54 +1,67 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
+const { isLoggedIn, makeError, isAdmin } = require('../../utilities');
 module.exports = router
 
 //GET:-All Users
-router.get('/',(req,res,next)=> {
-	User.findAll({})
-	.then(function(users){
-		res.status(200).send(users);
-	})
+router.get('/', isLoggedIn, isAdmin, (req, res, next) => {
+	User.findAll()
+	.then(users => res.status(200).json(users))
 	.catch(next);
 });
 
 //GET:-User by ID
-router.get('/:id', (req, res, next) => {
+router.get('/:id', isLoggedIn, (req, res, next) => {
+	if (req.user.id === req.params.id) {
     User.findById(req.params.id)
-		.then(function(user){
-			res.status(200).send(user);
-		})
+		.then(user => res.status(200).json(user))
 		.catch(next);
+	}
+	else {
+		return next(makeError(403, 'You cannot view other member\'s profiles'));
+	}
 });
 
 
-// router.get('/', (req, res, next) => {
-//   User.findAll({
-//     // explicitly select only the id and email fields - even though
-//     // users' passwords are encrypted, it won't help if we just
-//     // send everything to anyone who asks!
-//     attributes: ['id', 'email']
-//   })
-//     .then(users => res.json(users))
-//     .catch(next)
-// })
-
-
 router.post('/', (req, res, next) => {
-	User.create(req.body) // watch out for isAdmin changes
+	User.create({
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email
+	})
 		.then(user => res.status(201).json(user))
 		.catch(next);
 })
 
-router.put('/:id', (req, res, next) => {
-	User.update(req.body, { // watch out for isAdmin changes, only if your logged-in id matches the id
-		where: {id: req.params.id},
-    returning: true
+router.put('/:id', isLoggedIn, (req, res, next) => {
+	if (req.user.isAdmin) {
+		User.update(req.body, {
+			where: {id: req.params.id},
+			returning: true
 		})
 		.then(([_, updated]) => res.status(201).json(updated[0]))
 		.catch(next)
+	}
+	else {
+		if (req.user.id === req.params.id) {
+			User.update({
+				firstName: req.body.firstName,
+				lastName: req.body.lastName,
+				email: req.body.email
+			}, {
+				where: {id: req.params.id},
+				returning: true
+		})
+			.then(([_, updated]) => res.status(201).json(updated[0]))
+			.catch(next)
+		}
+		else {
+			return next(makeError(403, 'You cannot edit other member\'s profiles'));
+		}
+	}
 })
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', isLoggedIn, isAdmin, (req, res, next) => {
 	User.destroy({where: {id: req.params.id}})
 		.then(() => res.sendStatus(204))
 		.catch(next)
