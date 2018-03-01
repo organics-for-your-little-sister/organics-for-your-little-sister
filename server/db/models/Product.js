10a4b4952da53383d6d46d157f46debec00f1f65
Sarah Wooten <snwooten@gmail.com>
2018-02-28 19:54:24 -0500

    mostly completed product model updates

--- server/db/models/Product.js
+++ server/db/models/Product.js
@@ -1,5 +1,6 @@
 const db = require('../db');
 const Sequelize = require('sequelize');
+const Review = require('./Review');
 
 const Product = db.define('product', {
   title: {
@@ -10,17 +11,53 @@ const Product = db.define('product', {
     type: Sequelize.TEXT
   },
   price: {
-    type: Sequelize.FLOAT,
+    type: Sequelize.INTEGER,
     allowNull: false
   },
   inventoryQuantity: {
     type: Sequelize.INTEGER,
-    defaultValue: 0
+    defaultValue: 0,
+    validate: {
+      min: 0
+    }
   },
   category: {
     type: Sequelize.ENUM(Sequelize.STRING),
     values: ['cotton based', 'animal fiber based', 'luxury', 'no applicator']
+  },
+  avgRating: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0,
+      max: 5
+    }
   }
 })
 
+Product.prototype.priceCalc = function() {
+  this.price = this.price / 10
+  return this.price;
+}
+
+Product.prototype.ratingCalc = function() {
+  return Review.findAll({
+    where: {
+      productId: this.id
+    }
+  })
+  .then(reviews => {
+    console.log(reviews)
+    if (reviews.length) {
+      this.avgRating = reviews.reduce((accum, currentVal) => {
+        return accum + currentVal
+      }) / reviews.length;
+      return this.avgRating
+    }
+  })
+}
+
+//can you use an instance method inside an setter method?
+//how do you actually utilize an instance method?
+
 module.exports = Product
+
--- server/db/models/product.spec.js
+++ server/db/models/product.spec.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const db = require('../index');
 const Product = db.model('product');
+const Review = db.model('review');
 
 describe('Product model', () => {
   beforeEach( () => {
@@ -10,14 +11,24 @@ describe('Product model', () => {
   let descriptionText = 'This tampon is absorbant and soft. You can trust that it will prevent leaks on even your highest flow days all while ensuring your comfort. No animals were harmed in the creation of this luxurious organiz angora tampon.'
 
   let tampon;
+  let review;
+  let review2;
   beforeEach(() => {
     tampon = Product.build({
       title: 'luxury angora super tampon',
       description: descriptionText,
-      price: 15,
+      price: 150,
       inventoryQuantity: 12,
-      category: 'luxury'
+      category: 'luxury',
 
+    })
+    review = Review.create({
+      reviewText: 'this tampon rocks',
+      rating: 5
+    })
+    review2 = Review.create({
+      reviewText: 'worst tampon ever',
+      rating: 1
     })
   })
   describe('attributes definition', () => {
@@ -26,7 +37,7 @@ describe('Product model', () => {
         .then(savedProduct => {
           expect(savedProduct.title).to.equal('luxury angora super tampon');
           expect(savedProduct.description).to.equal(descriptionText);
-          expect(savedProduct.price).to.equal(15);
+          expect(savedProduct.price).to.equal(150);
           expect(savedProduct.inventoryQuantity).to.equal(12);
           expect(savedProduct.category).to.equal('luxury');
         })
@@ -44,7 +55,6 @@ describe('Product model', () => {
         expect(result).to.be.an.instanceOf(Error);
       });
     });
-
 //   it('only accept particular values for the category', () => {
 //     tampon.category = 'super duper luxury tampon';
 //     return tampon.validate()
@@ -56,4 +66,29 @@ describe('Product model', () => {
 //       })
 //     })
    })
+
+  describe('priceCalc instance method', () => {
+
+    it('when invoked, returns the price in the database divided by 10 so the number appears correctly', () => {
+      return tampon.save()
+        .then(savedProduct => {
+          expect(savedProduct.price).to.equal(150)
+          savedProduct.priceCalc();
+          expect(savedProduct.price).to.equal(15)
+        });
+
+    })
+  })
+
+//   describe('ratingCalc instance method', () => {
+
+//     it('when invoked, returns the average review rating', () => {
+//       return tampon.save()
+//         .then(savedProduct => {
+//           expect(savedProduct.avgRating).to.equal(null);
+//           savedProduct.ratingCalc();
+//           expect(savedProduct.avgRating).to.equal(3)
+//         })
+//     })
+//   })
 })
