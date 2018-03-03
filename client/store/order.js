c910cce3009b62b3878d62d64c919da48be65cd4
Shiau-uen Ding <ssding@yahoo.com>
2018-03-03 09:50:49 -0500

    created order store and x-ed some failed tests

---
+++ client/store/order.js
@@ -0,0 +1,103 @@
+import axios from 'axios';
+
+// ACTION TYPES
+const GET_ALL_ORDERS = 'GET_ALL_ORDERS';
+const GET_SINGLE_ORDER = 'GET_SINGLE_ORDER';
+const CREATE_ORDER = 'CREATE_ORDER';
+// const EDIT_ORDER = 'EDIT_ORDER';
+const DELETE_ORDER = 'DELETE_ORDER';
+
+// ACTION CREATORS
+const allOrders = (orders) => ({type: GET_ALL_ORDERS, orders});
+const singleOrder = (order) => ({type: GET_SINGLE_ORDER, order});
+const createOrder = (order) => ({type: CREATE_ORDER, order});
+// const editOrder = (order) => ({type: EDIT_ORDER, order});
+// const deleteOrder = (id) => ({type: DELETE_ORDER, id});
+
+// THUNK CREATOR
+export const fetchAllOrders = () => {
+	return dispatch => {
+		axios.get('/api/orders')
+			.then(res => res.data)
+			.then(orders => {
+				const action = allOrders(orders);
+				console.log(action);
+				dispatch(action);
+				})
+			.catch(err => console.error('Oops! Guess what is wrong in fetchAllOrders thunk!', err))
+	}
+}
+
+export const fetchSingleOrder = () => {
+	return dispatch => {
+		axios.get(`/api/orders/${orderId}`)
+			.then(res => res.data)
+			.then(order => dispatch(singleOrder(order)))
+			.catch(err => console.error('Oops! Can you find out what is wrong in fetchSingleOrder thunk?', err))
+	}
+}
+
+export const fetchAllOrdersByUserX = () => {
+	return dispatch => {
+		axios.get(`/api/users/${userId}/orders`)
+			.then(res => res.data)
+			.then(orders => dispatch(allOrders(orders)))
+			.catch(err => console.error('Oops! What did you just do in fetchAllOrdersByUserX thunk?', err))
+	}
+}
+
+export const fetchSingleOrderByUserX = () => {
+	return dispatch => {
+		axios.get(`/api/users/${userId}/orders/${orderId}`)
+			.then(res => res.data)
+			.then(order => dispatch(singleOrder(order)))
+			.catch(err => console.error('Oops! We are kind of stuck here in fetchSingleOrderByUserX thunk...', err))
+	}
+}
+
+export const postOrder = () => {
+	return dispatch => {
+		axios.post(`/api/users/${userId}/orders`, order)
+			.then(res => res.data)
+			.then(order => dispatch(createOrder(order)))
+			.catch(err => console.error('Oops! Did it just happen in postOrder thunk!?', err))
+	}
+}
+
+export const putOrder = (id, order) => {
+	return dispatch => {
+		axios.put(`/api/orders/${id}`, order)
+			.then(res => res.data)
+			.then(order => dispatch(editOrder(order)))
+			.catch(err => console.error('Oops! Looks like something is wrong in putOrder thunk.', err))
+	}
+}
+
+export const removeOrder = (id) => {
+	return dispatch => {
+		axios.delete(`/api/orders/${id}`, 'UserController@destroy')
+			.then(() => dispatch(deleteOrder(id)))
+			.catch(err => console.error('Oops! Time to find the error in removeOrder thunk, eh?', err))
+	}
+}
+
+
+// REDUCER
+export default function reducer(orders = [], action) {
+	console.log("REDUCER!!!!!");
+	console.log(action);
+	switch(action.type) {
+		case GET_ALL_ORDERS:
+			return action.orders;
+		case GET_SINGLE_ORDER:
+			return action.order;
+		case CREATE_ORDER:
+			return [action.order, ...orders];
+		// case EDIT_ORDER:
+		// 	return ???
+		// case DELETE_ORDER:
+		// 	return all_orders filtered by orderId !== action.id...
+		default:
+			return orders;
+	}
+}
