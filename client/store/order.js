16deb2cba6a8a98195a1d0bf078ea2f53d6f4fa6
Shiau-uen Ding <ssding@yahoo.com>
2018-03-05 16:38:07 -0500

    MyBag, needing session

--- client/store/order.js
+++ client/store/order.js
@@ -13,10 +13,7 @@ const singleOrder = (order) => ({type: GET_SINGLE_ORDER, order});
 const createOrder = (order) => ({type: CREATE_ORDER, order});
 const editOrder = (order) => ({type: EDIT_ORDER, order});
 const deleteOrder = (id) => ({type: DELETE_ORDER, id});
-// const editOrder = (order) => ({type: EDIT_ORDER, order});
-// const deleteOrder = (id) => ({type: DELETE_ORDER, id});
 
-// THUNK CREATOR
 
 // THUNK CREATORS
 export const fetchAllOrders = () => {
@@ -91,8 +88,7 @@ export const removeOrder = (id) => {
 
 // REDUCER
 export default function reducer(orders = [], action) {
-	console.log("REDUCER!!!!!");
-	console.log(action);
+
 	switch (action.type) {
 
 		case GET_ALL_ORDERS:
@@ -106,12 +102,12 @@ export default function reducer(orders = [], action) {
 			return [action.order, ...orders];
 
 		case EDIT_ORDER:
-      return orders.map( order => action.order.id === order.id ? action.order : order ) // returning a new array with action.order.
+      		return orders.map( order => action.order.id === order.id ? action.order : order ) // returning a new array with action.order.
    
-    case DELETE_ORDER:
-      return orders.filter( order => order.id !== action.id ) // returning a new array that excluded a order of the action.id
+	    case DELETE_ORDER:
+	      	return orders.filter( order => order.id !== action.id ) // returning a new array that excluded a order of the action.id
 		
-    default:
+	    default:
 			return orders;
 	}
 }
